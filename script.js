1956ab40dded197645d2d42053f4c13cd517e6cf
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 11:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  let table = document.getElementById("sampleTable");
+	let newRow = table.insertRow(0);
+	let newCell1 = newRow.insertCell();
+	let newCell2 = newRow.insertCell();
+	newCell1.innerText = "New Cell1";
+	newCell2.innerText = "New Cell2";
+	
   
 }
